c41c04dadecdb22206339317f3153da836dc20d4
BryanRosillo <108194629+BryanRosillo@users.noreply.github.com>
2025-01-26 16:46:24 -0500

    chore: first commit

---
+++ src/main/java/com/bsrg/micro/song/MicroSongApplication.java
@@ -0,0 +1,13 @@
+package com.bsrg.micro.song;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroSongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroSongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsrg/micro/song/MicroSongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsrg.micro.song;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroSongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
